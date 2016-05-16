46d7590126e4b004456ad5faa93c62d12da5e156
Chris Lankford <christopherlankford@gmail.com>
2016-05-16 11:11:53 -0700

    Created skeleton for sandbox project.

---
+++ app/ts/assets.ts
@@ -0,0 +1,9 @@
+/*
+ * We're using webpack which means we have to explicitly require all of the
+ * assets we want to use.
+ */
+require('../css/styles.scss');
+require('../css/vendor/semantic.min.css');
+require('../css/vendor/themes/default/assets/fonts/icons.woff2');
+require('../css/vendor/themes/default/assets/fonts/icons.woff');
+require('../css/vendor/themes/default/assets/fonts/icons.ttf');
