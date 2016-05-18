109dddd3bec4caac3ede675176a98175b71a860f
Chris Lankford <christopherlankford@gmail.com>
2016-05-18 11:14:22 -0700

    Implemented an example of no style encapsulation

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -3,14 +3,22 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { InlineStyle } from './components/InlineStyleComponent';
 import { ExternalStyle } from './components/ExternalStyleComponent';
 import { NativeEncapsulation } from './components/NativeEncapsulationComponent';
+import { NoEncapsulation } from './components/NoEncapsulationComponent';
 
 @Component({
     selector: 'sandbox-app',
-    directives: [ InlineStyle, ExternalStyle, NativeEncapsulation ],
+    directives: 
+    [ 
+        InlineStyle, 
+        ExternalStyle, 
+        NativeEncapsulation,
+        NoEncapsulation
+    ],
     template: `
     <inline-style></inline-style>
     <external-style></external-style>
     <native-encapsulation></native-encapsulation>
+    <!-- <no-encapsulation></no-encapsulation> -->
     `
 })
 class SandboxApp {
